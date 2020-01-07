d33a26d8855e6e88cc1e5d8cc28e40fe660494d5
Thisnthat <thisnthat@mgmcomp.net>
2020-01-07 14:06:24 -0600

    Added a customer GuildMemberEdit method to handled nick and roles together

--- restapi.go
+++ restapi.go
@@ -831,6 +831,26 @@ func (s *Session) GuildMemberDeleteWithReason(guildID, userID, reason string) (e
 	return
 }
 
+// GuildMemberEditCustom updates a members roles and nick
+// guildID  : The ID of a Guild.
+// userID   : The ID of a User.
+// nick     : The new nickname to set on the
+// roles    : A list of role ID's to set on the member.
+func (s *Session) GuildMemberEditCustom(guildID, userID, nick string, roles []string) (err error) {
+
+	data := struct {
+		Nick  string   `json:"nick,omitempty"`
+		Roles []string `json:"roles,omitempty"`
+	}{nick, roles}
+	fmt.Println(data)
+	_, err = s.RequestWithBucketID("PATCH", EndpointGuildMember(guildID, userID), data, EndpointGuildMember(guildID, ""))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 // GuildMemberEdit edits the roles of a member.
 // guildID  : The ID of a Guild.
 // userID   : The ID of a User.
